2491e6ed918977c7e732212dbb1a66afb416cf28
Greg Orvik <gvorvik@gmail.com>
2018-04-20 16:28:43 -0500

    click handlers set up

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,29 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('JQuery is loaded');
+    $('#addButton').on('click', addNumbers);
+    $('#subtractButton').on('click', subtractNumbers);
+    $('#multiplyButton').on('click', multiplyNumbers);
+    $('#divideButton').on('click', divideNumbers);
+}
+
+
+function addNumbers() {
+    console.log('a button click');
+
+}
+
+function subtractNumbers() {
+    console.log('s button click');
+
+}
+
+function multiplyNumbers() {
+    console.log('m button click');
+
+}
+
+function divideNumbers() {
+    console.log('d button click');
+
 }
